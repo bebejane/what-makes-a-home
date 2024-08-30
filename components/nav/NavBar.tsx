d71f62573b6c8782d6a51ffc34d0269a74b1d5f9
Bebe Jane <bjornthief@gmail.com>
2024-08-30 14:20:21 +0200

    mobile

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import type { Menu } from "../../lib/menu";
 import { useScrollInfo } from 'next-dato-utils/hooks'
+import { Spin as Hamburger } from 'hamburger-react'
 
 export type Props = {
   menu: Menu
@@ -21,21 +22,30 @@ export default function NavBar({ menu, }: Props) {
   const [isScrolledDown, setIsScrolledDown] = useState(false)
   const [showNewsletter, setShowNewsletter] = useState(false)
 
+  const handleClick = () => {
+    setOpen(false)
+  }
+
   return (
-    <nav className={s.navbar}>
-      <ul>
-        {menu.map((item, i) => {
-          const active = pathname === item.slug
-          return (
-            <li key={i} className={cn(active && s.active)}>
-              <Link href={item.slug}>
-                {item.label}
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
-    </nav>
+    <>
+      <nav className={cn(s.navbar, open && s.open)}>
+        <ul>
+          {menu.map((item, i) => {
+            const active = pathname === item.slug
+            return (
+              <li key={i} className={cn(active && s.active)}>
+                <Link href={item.slug} onClick={handleClick}>
+                  {item.label}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+      <nav className={s.hamburger}>
+        <Hamburger toggled={open} toggle={setOpen} size={28} />
+      </nav>
+    </>
   );
 }
 
