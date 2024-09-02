df79af65012311473e63efb04c3cbb75b1eea8bb
Bebe Jane <bjornthief@gmail.com>
2024-09-02 16:35:40 +0200

    fix2

--- app/(datocms)/api/web-previews/route.ts
+++ app/(datocms)/api/web-previews/route.ts
@@ -7,8 +7,7 @@ export const runtime = "edge"
 export async function POST(req: NextRequest) {
 
   return await webPreviews(req, async ({ item, itemType }) => {
-    const path = await buildRoute(itemType.api_key, item.attributes)
-    console.log(path)
+    const path = await buildRoute(itemType.attributes.api_key, item.attributes)
     return path
   })
 }
