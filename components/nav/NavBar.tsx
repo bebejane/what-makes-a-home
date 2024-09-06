0b62faabda7903804af9289fc93309e53823ae39
Bebe Jane <bjornthief@gmail.com>
2024-09-06 08:50:01 +0200

    revert

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect } from "react";
+import React from "react";
 import s from './NavBar.module.scss'
 import cn from 'classnames'
 import Link from "next/link";
@@ -9,7 +9,6 @@ import { usePathname } from "next/navigation";
 import type { Menu } from "../../lib/menu";
 import { Fade as Hamburger } from 'hamburger-react'
 import { useStore } from "../../lib/store";
-import path from "path";
 
 export type Props = {
   menu: Menu
@@ -19,36 +18,18 @@ export default function NavBar({ menu, }: Props) {
 
   const pathname = usePathname()
   const [open, setOpen] = useState(false)
-  const [inIntro, hoveringNav, setHoveringNav] = useStore(state => [state.inIntro, state.hoveringNav, state.setHoveringNav]);
+  const [inIntro] = useStore(state => [state.inIntro]);
 
-  const handleClick = () => {
-    setTimeout(() => setOpen(false), 300)
-  }
-
-  const handleMouse = (e: React.MouseEvent) => {
-    setHoveringNav(e.type === 'mouseenter')
-  }
-
-  useEffect(() => {
-    setHoveringNav(false)
-  }, [pathname])
-
-  useEffect(() => {
-    document.body.classList.toggle('dark', hoveringNav)
-  }, [hoveringNav])
+  const handleClick = () => setTimeout(() => setOpen(false), 300)
 
   return (
     <>
-      <nav
-        className={cn(s.navbar, open && s.open, inIntro && s.intro, hoveringNav && s.hovering)}
-      //onMouseEnter={handleMouse}
-      //onMouseLeave={handleMouse}
-      >
+      <nav className={cn(s.navbar, open && s.open, inIntro && s.intro)}>
         <ul>
           {menu.map((item, i) => {
             const active = pathname === item.slug
             return (
-              <li key={i} className={cn(active && s.active)} onMouseEnter={handleMouse} onMouseLeave={handleMouse}>
+              <li key={i} className={cn(active && s.active)}>
                 <Link href={item.slug} onClick={handleClick} >
                   {item.label}
                 </Link>
